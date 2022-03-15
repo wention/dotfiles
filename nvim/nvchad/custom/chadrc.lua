a71f1d333320478a5ccfa888eba297ba30adce74
wention <xuyi.828@gmail.com>
2022-03-15 11:51:26 +0800

    add nvim config

---
+++ nvim/nvchad/custom/chadrc.lua
@@ -0,0 +1,30 @@
+local M = {}
+local userPlugins = require "custom.plugins"
+
+M.options = {
+   mapleader = ",",
+   tabstop = 4,
+}
+
+M.ui = {}
+
+M.plugins = {
+  install = userPlugins,
+
+  options = {
+    lspconfig = {
+      setup_lspconf = "custom.plugins.lspconfig",
+    },
+  },
+}
+
+M.mappings = {}
+
+M.mappings.plugins = {
+   nvimtree = {
+      toggle = "<leader>e",
+      focus = "",
+   },
+}
+
+return M
---
+++ nvim/nvchad/custom/plugins/init.lua
@@ -0,0 +1,4 @@
+return {
+  { "williamboman/nvim-lsp-installer" },
+  { "simrat39/symbols-outline.nvim" },
+}
